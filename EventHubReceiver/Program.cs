f7d9d1e0e4be5b6dc1f5c24437c8f588760ff947
Sathish Krishnan <sathishkumar.krishnan@hotmail.com>
2017-06-22 13:58:24 -0400

    Sample reference implemenation of Event Processor Host for receiving messages from EventHub.

---
+++ EventHubReceiver/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.ServiceBus.Messaging;
+
+namespace EventHub.Samples.EPHReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventHubPath = ConfigurationManager.AppSettings["EventHubName"];
+            var consumerGroupName = ConfigurationManager.AppSettings["ConsumerGroupName"];
+            var eventHubConnectionString = ConfigurationManager.AppSettings["EventHubConnectionString"];
+            var storageConnectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+            
+            var eph = new EventProcessorHost(eventHubPath, consumerGroupName, eventHubConnectionString, storageConnectionString);
+
+            // Default options can be overridden by a new instance of EventProcessorOptions with configurable values.
+            var options = EventProcessorOptions.DefaultOptions;
+            options.ExceptionReceived += EventProcessor_ExceptionReceived;
+
+            Console.WriteLine("Starting event processor host.");
+            eph.RegisterEventProcessorAsync<SampleEventProcessor>(EventProcessorOptions.DefaultOptions)
+                .ContinueWith(t => Console.WriteLine("Event processor host started.")).GetAwaiter().GetResult();
+
+            // This will keep the process running, until manually stopped.
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+
+            Console.WriteLine("Stopping event processor host.");
+            eph.UnregisterEventProcessorAsync()
+                .ContinueWith(t => Console.WriteLine("Event processor host stopped.")).GetAwaiter().GetResult();
+        }
+
+        private static void EventProcessor_ExceptionReceived(object sender, ExceptionReceivedEventArgs e)
+        {
+            // Handle event processor failures here.
+
+            Console.WriteLine($"Error occured in event processor. \n {e.Exception}");
+        }
+    }
+}
---
+++ EventHubReceiver/SampleEventProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace EventHub.Samples.EPHReceiver
+{
+    public class SampleEventProcessor : IEventProcessor
+    {
+        public async Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            // Add any cleanup logic here
+
+            if (reason == CloseReason.Shutdown)
+            {
+                await context.CheckpointAsync();
+            }
+            
+            Console.WriteLine($"Stopped listening on Parition - {context.Lease.PartitionId}");
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine($"Started listening on Parition - {context.Lease.PartitionId}");
+
+            // Add any startup logic here
+
+            return Task.FromResult<object>(null);
+        }
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            Console.WriteLine($"Received {messages.Count()} messages from Partition - {context.Lease.PartitionId}");
+
+            // Add your processing logic here
+
+            await context.CheckpointAsync().ConfigureAwait(false);
+        }
+    }
+}
